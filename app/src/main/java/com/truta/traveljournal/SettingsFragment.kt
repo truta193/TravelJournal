c196288bddd73b011afead4792dacd8da6b96e6b
truta193 <trutaandrei7033@gmail.com>
2024-01-04 23:20:10 +0200

    Preferences theme

--- app/src/main/java/com/truta/traveljournal/SettingsFragment.kt
+++ app/src/main/java/com/truta/traveljournal/SettingsFragment.kt
@@ -1,21 +1,43 @@
 package com.truta.traveljournal
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    fun applyTheme(mode: String, context: Context) {
+        if (context.getString(R.string.settings_theme_value_dark) == mode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else if (context.getString(R.string.settings_theme_value_light) == mode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         return super.onCreateView(inflater, container, savedInstanceState)
 
     }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        preferenceManager.findPreference<Preference>(getString(R.string.settings_theme_key))?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { preference: Preference?, newValue: Any? ->
+                applyTheme(newValue as String, requireContext())
+                true
+            }
     }
 }
