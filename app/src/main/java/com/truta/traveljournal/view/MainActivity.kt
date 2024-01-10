a92f89fff12cdb77e8d6e34658f2043a439ab1d1
truta193 <trutaandrei7033@gmail.com>
2024-01-10 17:19:28 +0200

    Remove test fragment

--- app/src/main/java/com/truta/traveljournal/view/MainActivity.kt
+++ app/src/main/java/com/truta/traveljournal/view/MainActivity.kt
@@ -74,7 +74,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.homeFragment -> navController.navigate(R.id.homeFragment)
-            R.id.testFragment -> navController.navigate(R.id.testFragment)
             R.id.settingsFragment -> navController.navigate(R.id.settingsFragment)
             R.id.aboutUsFragment -> navController.navigate(R.id.aboutUsFragment)
             R.id.contactUs -> {
